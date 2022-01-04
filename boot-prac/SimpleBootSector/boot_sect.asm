d0f445b8d38bf240b610491993b7377463c4970e
Jake Imanaka <jake.imanaka@gmail.com>
2022-01-04 11:13:05 -1000

    asdf

--- boot-prac/SimpleBootSector/boot_sect.asm
+++ boot-prac/SimpleBootSector/boot_sect.asm
@@ -1,6 +1,19 @@
-loop:
+; Printing Hello
 
-	jmp loop
+mov ah, 0x0e	; scrolling teletype BIOS routine
+
+mov al, 'H'	; BIOS teletype prints from al
+int 0x10	; print interrupt
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
 
 times 510-($-$$) db 0
 
